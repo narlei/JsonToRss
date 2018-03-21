e4259be3c6b9bd8cacefc178ef241c5117f85bb1
Narlei Moreira <narlei.guitar@gmail.com>
2018-03-21 17:50:11 -0300

    first commit

---
+++ JsonToRss/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  JsonToRss
+//
+//  Created by Narlei A Moreira on 21/03/18.
+//  Copyright © 2018 Narlei A Moreira. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ JsonToRss/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  JsonToRss
+//
+//  Created by Narlei A Moreira on 21/03/18.
+//  Copyright © 2018 Narlei A Moreira. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
